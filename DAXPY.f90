386d57d817f6b753b9145cd05e69f79736823760
Briley James <brileyj@cheyenne5.ib0.cheyenne.ucar.edu>
2020-01-23 16:30:57 -0700

    DAXPY executed for Matricies

--- DAXPY.f90
+++ DAXPY.f90
@@ -1,19 +1,23 @@
 ! DAXPY program     Y=A*X+Y where X and Y are vectors    Jan 23  2020
 !
 ! 
-      program DAXPY_Test 
+      program DAXPY_Test
+      implicit none 
+ 
       !Define Variables 
       real,parameter::A=2              !constant multiplier 
-      integer :: i,Xsize
-      integer,parameter::n=5
-      real,dimension(n)::X,Y,Yinv    !square Matricies 
+      integer :: i,Xsize,j
+      integer,parameter::n=4
+      real,dimension(n,n)::X,Y,Yinv    !square Matricies 
       
-      Xsize=size(X)
-
-      !Populate Matrix Z,Y 
-      do i=1,Xsize 
-        X(i)=i
-        Y(i)=2*i
+      Xsize=size(X,1)
+       
+      !Populate Matrix X,Y 
+      do i=1,Xsize
+         do j=1,Xsize 
+            X(i,j)=i
+            Y(i,j)=j
+         end do 
       end do 
     
       !Print Matrix to check
@@ -25,21 +29,29 @@
       !Print the Output from DAXPY function
       print*,"DAXPY Output Matrix Y:",Y
 
+      !Inverse new matrix using LAPACK
+      !call dggetrf(Xsize, ,Yinv,ipiv,info)
+      !call dgetri(n,n,Yinv,n, )
+           !dgetri parameters: N,A,LDA,IPIV,Work,Lwork,info
+
       end program DAXPY_Test 
 
 
       subroutine  DAXPY(A,X,Y,Xsize)
-      real,intent(in)::X(*)
-      real,intent(inout)::Y(*)
-      integer,intent(in)::Xsize
+      implicit none 
+      real,intent(in)::X(Xsize,Xsize)
+      real,intent(inout)::Y(Xsize,Xsize)
       real,intent(in)::A
+      integer,intent(in)::Xsize
+      integer::i,j
 
-        if (A>0) then
+        if (A>0) then 
+          do i=1,Xsize
+          do j=1,Xsize
           !Scalar Multiplication then addition
-          do i=1, Xsize
-             Y(i)=A*X(i)+Y(i)
-          end do
-        end if
-        return 
+          Y(j,i)=A*X(j,i)+Y(j,i)
+          end do 
+          end do 
+        end if 
       end subroutine DAXPY
 
