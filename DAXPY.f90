27b5afafd4918e99c69135e1089b30ba62ee7ebb
Briley James <brileyj@cheyenne4.ib0.cheyenne.ucar.edu>
2020-01-23 12:27:37 -0700

    Version1 No Modifications

--- DAXPY.f90
+++ DAXPY.f90
@@ -1,22 +1,7 @@
 ! DAXPY program     Y=A*X+Y where X and Y are vectors    Jan 23  2020
 !
-!
-      real function DAXPY(A,X,Y,Xsize) 
-        if (A>0) then 
-          !Scalar Multiplication 
-          do i=1, Xsize 
-             X(i)=A*X(i) 
-          end do 
-
-          !Matrix Addition 
-          do i=1, Xsize 
-             Y(i)=X(i)+Y(i)
-          end do 
-        end if 
-        DAXPY=Y 
-      end function DAXPY
-
-      program DAXPY_1 
+! 
+      program DAXPY_Test 
       !Define Variables 
       real,parameter::A=2              !constant multiplier 
       integer :: i,Xsize,Ysize
@@ -38,7 +23,25 @@
       print*,"Y Matrix is:",Y 
 
       !Function Call 
-      print*,"DAXPY Output Matrix Y:",DAXPY(A,X,Y,Xsize)
+      call DAXPY(A,X,Y,Xsize)
+      !Print the Output from DAXPY function
+      print*,"DAXPY Output Matrix Y:",Y
 
-      end program DAXPY_1 
+      end program DAXPY_Test 
+
+
+      subroutine  DAXPY(A,X,Y,Xsize)
+      real,intent(in)::X(*)
+      real,intent(inout)::Y(*)
+      integer,intent(in)::Xsize
+      real,intent(in)::A
+
+        if (A>0) then
+          !Scalar Multiplication then addition
+          do i=1, Xsize
+             Y(i)=A*X(i)+Y(i)
+          end do
+        end if
+        return 
+      end subroutine DAXPY
 
